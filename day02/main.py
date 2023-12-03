7583a7df442994b3c5908cc16e5a868e05e01196
FahdAzouz <fahdstudies00@gmail.com>
2023-12-03 21:34:55 +0100

    update

--- day02/main.py
+++ day02/main.py
@@ -3,11 +3,28 @@ import re
 
 def func():
     sum = 0
-    with open("input", "r") as data:
-        for line in data:
+    with open("input.txt", "r") as f:
+        for line in f:
             if possibility(line):
-                sum =+ int(line[5:7])
+                sum += int(line[5:7])
     print(sum)
 
 def possibility(line):
     arr = re.split(", |;", line)
+    for string in arr:
+        if "red" in string:
+            for s in string.split():
+                if s.isdigit() and int(s) > 12:
+                    return False
+        if "green" in string:
+            for s in string.split():
+                if s.isdigit() and int(s) > 13:
+                    return False
+        if "blue" in string:
+            for s in string.split():
+                if s.isdigit() and int(s) > 14:
+                    return False
+    return True
+
+if __name__ == "__main__":
+    func()
