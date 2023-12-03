b919a05c23fd6ccbc347e63292b098081d64248d
FahdAzouz <fahdstudies00@gmail.com>
2023-12-03 22:07:05 +0100

    ongoing

--- day02/main.py
+++ day02/main.py
@@ -1,14 +1,21 @@
 import re 
-
+import os
 
 def func():
-    sum = 0
-    with open("input.txt", "r") as f:
-        for line in f:
-            if possibility(line):
-                sum += int(line[5:7])
-    print(sum)
+    power = 0
+    highred = 0
+    highgreen = 0
+    highblue = 0
+    file_path = "/home/fahd/adventcode_challenge/day02/input.txt"
+    if os.path.exists(file_path):
+        with open(file_path, "r") as f:
+            for line in f:
+                highred, highgreen, highblue = function(line, highred, highgreen, highblue)
+                power = power + highred * highgreen * highblue
+        print(power)
 
+'''
+Part 1
 def possibility(line):
     arr = re.split(", |;", line)
     for string in arr:
@@ -25,6 +32,26 @@ def possibility(line):
                 if s.isdigit() and int(s) > 14:
                     return False
     return True
+'''
+
+# Part 2
+def function(line, highred, highgreen, highblue):
+    arr = re.split(", |;", line)
+    for string in arr:
+        if "red" in string:
+            for s in string.split():
+                if s.isdigit() and int(s) > highred:
+                    highred = int(s)
+        if "green" in string:
+            for s in string.split():
+                if s.isdigit() and int(s) > highgreen:
+                    highgreen = int(s)
+
+        if "blue" in string:
+            for s in string.split():
+                if s.isdigit() and int(s) > highblue:
+                    highblue = int(s)
+    return highred, highgreen, highblue
 
 if __name__ == "__main__":
     func()
